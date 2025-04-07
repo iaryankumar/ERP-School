1fa30738b52ae2fb7e8c73db4a8d1b76d96d2f76
iaryankumar <k.aryan5716@gmail.com>
2025-04-07 10:02:07 +0530

    ho gaya

--- frontend/teachers/src/components/Navbar.jsx
+++ frontend/teachers/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { RiDashboardFill } from 'react-icons/ri'
 
 const Navbar = (props) => {
     return (
-        <nav className='h-[85px] w-full border-b border-borderSecondary bg-white flex items-center justify-between sm:px-9 px-4 sticky top-[-1px]'>
+        <nav className='h-[65px] w-full border-b border-borderSecondary bg-white flex items-center justify-between sm:px-9 px-4 sticky top-[-1px]'>
             {<div className='flex items-center justify-center gap-1'>
                 <RiDashboardFill className='text-xl text-textSecondary mt-0.5' />
                 <h1 className='text-xl font-bold text-textSecondary'>{props.title}</h1>
